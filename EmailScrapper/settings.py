e759a3e2026ca19ca6531a464cc63a92ce8fbff7
Sandeep Maurya <svf.sandeep@gmail.com>
2025-07-19 18:08:52 +0530

    reset settings .py

--- EmailScrapper/settings.py
+++ EmailScrapper/settings.py
@@ -54,6 +54,12 @@ DATABASES = {
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'scraper',
 ]
 
