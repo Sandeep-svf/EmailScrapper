4a747bf1267bc74d69750d82b03bf017fc64723a
Sandeep Maurya <svf.sandeep@gmail.com>
2025-07-19 18:16:52 +0530

    fixed session

--- EmailScrapper/scraper.py
+++ EmailScrapper/scraper.py
@@ -21,14 +21,20 @@ def get_next_batch_name():
     last_number = int(last_batch.name.replace("RBSH", ""))
     return f"RBSH{str(last_number + 1).zfill(3)}"
 
+
+
+
 def get_stealth_driver():
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
 
     options = uc.ChromeOptions()
-    options.headless = True
+    options.add_argument("--headless=new")  #  Important for Chrome 137+
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--remote-debugging-pipe")  #  Helps pipe communication
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     options.add_argument(f"user-agent={user_agent}")
     options.add_argument("--start-maximized")
 
@@ -43,6 +49,7 @@ def get_stealth_driver():
             fix_hairline=True)
 
     return driver
+
 
 def scrape_job(job):
     driver = None
